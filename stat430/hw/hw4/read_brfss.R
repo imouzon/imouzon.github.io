868b1eca1d7ab653a6b94b095d08af2badf5bec8
Ian Mouzon <ian.mouzon@gmail.com>
2017-10-24 12:32:05 -0500

    adding hw4

---
+++ stat430/hw/hw4/read_brfss.R
@@ -0,0 +1,22 @@
+#--------------------------------------**--------------------------------------#
+#  File Name: read_brfss.R
+#  Purpose:
+#  Creation Date:
+#  Created By:
+#--------------------------------------**--------------------------------------#
+read_brfss <- function(path_to_brfss_file){
+
+   path_to_brfss_fwf_layout = "http://imouzon.github.io/stat430/hw4/brfss_column_layout.tsv"
+
+   fwf_layout <- read.delim(
+      path_to_brfss_column_layout,
+      stringsAsFactors = FALSE
+   )
+
+   d <- read.fwf(
+      path_to_brfss_file,
+      widths= c(fwf_layout[-1,1] - fwf_layout[-nrow(fwf_layout),1],1),
+      col.names=fwf_layout$varname
+   )
+
+}
