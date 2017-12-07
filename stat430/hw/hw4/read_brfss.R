237c0bb18f14002f742687e3c90cf9138b45b220
Ian Mouzon <ian.mouzon@gmail.com>
2017-12-06 21:49:17 -0600

    adding

--- stat430/hw/hw4/read_brfss.R
+++ stat430/hw/hw4/read_brfss.R
@@ -4,7 +4,7 @@
 #  Creation Date:
 #  Created By:
 #--------------------------------------**--------------------------------------#
-read_brfss <- function(path_to_brfss_file){
+read_brfss_by_state <- function(path_to_brfss_file, states_to_read=NULL){
 
    path_to_brfss_fwf_layout = "http://imouzon.github.io/stat430/hw/hw4/brfss_column_layout.tsv"
 
@@ -13,10 +13,33 @@ read_brfss <- function(path_to_brfss_file){
       stringsAsFactors = FALSE
    )
 
-   d <- read.fwf(
-      path_to_brfss_file,
-      widths= c(fwf_layout[-1,1] - fwf_layout[-nrow(fwf_layout),1],1),
-      col.names=fwf_layout$varname
-   )
+   d <- lapply(1:486303, function(i){
+
+
+      d <- read.fwf(
+         skip = i-1,
+         n=1,
+         path_to_brfss_file,
+         widths= c(fwf_layout[-1,1] - fwf_layout[-nrow(fwf_layout),1],1),
+         col.names=fwf_layout$varname
+      )
+
+      if(d[,1] %in% states_to_read){
+
+         return(d)
+
+      }else{
+
+         return(NULL)
+
+      }
+
+   })
+
+   d <- do.call("rbind", d)
+
+   return(d)
 
 }
+
+
